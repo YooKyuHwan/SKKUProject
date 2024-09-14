b51c3fff854dde1358d38922bf109509b391da27
yookyuhawn <yookyuhawn@naver.com>
2024-09-15 02:02:04 +0900

    remake my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function MyComponent(){
+  return (
+    <h>HOME</h>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent></MyComponent>
+    </div>
+  );
+}
+
+export default App;
